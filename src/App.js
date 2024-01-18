418f37eb460d2cb7ed6bfab6f490c98468a6bdf0
carolinacartaxo <carolinamcartaxo@gmail.com>
2024-01-18 19:22:50 -0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <h1> Weather app </h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://boisterous-naiad-a83020.netlify.app/">
+          {" "}
+          Carolina Cartaxo
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/carolinacartaxo/react-weather-project"
+          target="_blank"
+          rel="noreferrer">
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
     </div>
   );
 }
