a0cf87bf048ed5db0363269899fbb7d7019d4fc4
carolinacartaxo <carolinamcartaxo@gmail.com>
2024-01-22 16:31:04 -0300

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather app </h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://boisterous-naiad-a83020.netlify.app/">
