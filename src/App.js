4e7ec9ba29bc9b03f87261de87b4f63cf7431846
carolinacartaxo <carolinamcartaxo@gmail.com>
2024-01-22 13:01:52 -0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Weather app </h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://boisterous-naiad-a83020.netlify.app/">
-          {" "}
-          Carolina Cartaxo
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/carolinacartaxo/react-weather-project"
-          target="_blank"
-          rel="noreferrer">
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather app </h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://boisterous-naiad-a83020.netlify.app/">
+            {" "}
+            Carolina Cartaxo
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/carolinacartaxo/react-weather-project"
+            target="_blank"
+            rel="noreferrer">
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
